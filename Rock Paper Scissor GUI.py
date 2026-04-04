b9f643dda168b3ef05663f3222a23a756befb8be
Rayyan <Rayyanshafqat@gmail.com>
2026-04-04 21:29:21 +0500

    initial

--- Rock Paper Scissor GUI.py
+++ Rock Paper Scissor GUI.py
@@ -1,36 +1,59 @@
-#Imports
-from tkinter import * 
+from tkinter import *
 import random
 
-#System
+# System
 root = Tk()
 root.title('RPS Arena')
-root.geometry('200x250')
+root.geometry('300x350')
+root.configure(bg="red")   # Background color
 
-#Choices
-choices = ['Rock','Paper','Scissor']
+# Choices
+choices = ['Rock', 'Paper', 'Scissors']
 
-#Conditions
+# Function
 def play(user):
-     computer = random.choice(choices)
-     if user == computer:
-          result =  'Draw'
-     elif(user == 'Rock' and computer == 'Scissor') or \
-         (user == 'Paper' and computer == 'Rock') or \
-         (user == 'Scissor' and computer == 'Paper'):
-         result = 'You Win😊'     
-     else:
-          result = 'You Lose😞'
+    computer = random.choice(choices)
 
-     label.config(text="You: " + user + "\nComputer: " + computer + "\n" + result)
+    if user == computer:
+        result = 'Draw'
+    elif (user == 'Rock' and computer == 'Scissors') or \
+         (user == 'Paper' and computer == 'Rock') or \
+         (user == 'Scissors' and computer == 'Paper'):
+        result = 'You Win 😊'
+    else:
+        result = 'You Lose 😞'
+
+    label.config(
+        text="You: " + user + "\nComputer: " + computer + "\n" + result
+    )
 
 # Buttons
-Button(root, text="Rock",  width=15, command=lambda: play("Rock")).pack(pady=10)
-Button(root, text="Paper"  , width=15, command=lambda: play("Paper")).pack(pady=10)  
-Button(root, text="Scissors" , width=15, command=lambda: play("Scissors")).pack(pady=10)
+btn_style = {
+    "width": 15,
+    "bg": "yellow",
+    "fg": "black",
+    "bd": 4,
+    "relief": "solid",
+    "highlightbackground": "darkblue"
+}
 
-#Results          
-label = Label(root, text="",font=("Arial", 25))
-label.pack(pady=25)
+Button(root, text="Rock", command=lambda: play("Rock"), **btn_style).pack(pady=10)
+Button(root, text="Paper", command=lambda: play("Paper"), **btn_style).pack(pady=10)
+Button(root, text="Scissors", command=lambda: play("Scissors"), **btn_style).pack(pady=10)
+
+# Result Label
+label = Label(
+    root,
+    text="",
+    font=("Arial", 14),
+    bg="red",
+    fg="yellow",
+    bd=4,
+    relief="solid",
+    highlightbackground="darkblue",
+    width=25,
+    height=4
+)
+label.pack(pady=20)
 
 root.mainloop()
