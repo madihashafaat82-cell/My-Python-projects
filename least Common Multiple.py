6e275ea588311951f3284d1bbfcba4b8446a3e40
Rayyan <Rayyanshafqat@gmail.com>
2026-04-23 19:52:51 +0500

    initial

---
+++ least Common Multiple.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+max_num = max(num1, num2)
+
+while True:
+    if max_num % num1 == 0 and max_num % num2 == 0:
+        print("LCM is:", max_num)
+        break
+    max_num += 1
