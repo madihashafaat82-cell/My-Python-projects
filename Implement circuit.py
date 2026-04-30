9dc85e26761ed22899ca86f8414a4b6603ee0573
Rayyan <Rayyanshafqat@gmail.com>
2026-04-30 13:04:16 +0500

    initial

---
+++ Implement circuit.py
@@ -0,0 +1,31 @@
+# Variables
+num1 = int(input('Enter your first number:'))
+num2 = int(input('Enter your second number:'))
+
+# BitWise Operations:
+# AND operator
+print('num1 AND(&) num2 = ',num1 & num2)
+
+# OR operator
+print('num1 OR(|) num2 = ',num1 | num2)
+
+# XOR operator
+print('num1 XOR(^) num2 = ',num1 ^ num2)
+
+# NOT operator with num1
+print('NOT(~) num1 = ',~ num1)
+
+# NOT operator with num2
+print('NOT(~) num2 = ',~ num2)
+
+# Right shift(>>) operator with num1
+print('num1 >> 1 = ',num1 >> 1)
+
+# Right shift(>>) operator with num2
+print('num2 >> 2 = ',num2 >> 2)
+
+# Left shift(<<) operator with num1
+print('num1 << 1 = ',num1 << 1)
+
+# Left shift(<<) operator with num2
+print('num1 << 2 = ',num1 << 2)
